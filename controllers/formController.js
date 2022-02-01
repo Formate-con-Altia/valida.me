5ef00270313f99a695d07caf275b6b26fde78769
Adrian <adriangonzalezfilgueira@gmail.com>
2022-02-01 11:15:57 +0100

    01/02/2022 Logro

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,76 +1,64 @@
 const Form = require("../models/forms");
-const mongoose = require("mongoose")
-const { parse } = require("himalaya")
+const mongoose = require("mongoose");
+const { parse } = require("node-html-parser");
 
 const createForm = (req, res) => {
-    console.log(req.params)
-    res.render("forms/index");
+  console.log(req.params);
+  res.render("forms/index");
 };
 
-const createNewForm = async(req, res) => {
-    const html = req.body.data
-    let data_;
-    let obj = []
+const createNewForm = async (req, res) => {
+  const parsedHtml = parse(req.body.data);
 
-    let fields = []
+  // Obtenemos el texto de todos los labels
+  const parsedLabels = parsedHtml
+    .querySelectorAll("label")
+    .map((elm) => elm.innerText);
 
-    input: [{
-        "name": ["name", "name"],
-        "email": 2,
-        "tel": 9,
-    }]
+  // Obtenemos los inputs y devolvemos un array de objetos con los atributos necesarios
+  const parsedInputs = parsedHtml
+    .querySelectorAll("input, textarea")
+    .map((elm, i) => {
+      return {
+        name: elm.attrs.name,
+        input: elm.attrs.type,
+        label: parsedLabels[i],
+      };
+    });
+  // const fieldSchema = new Schema({
+  //     input: {
+  //         type: String,
+  //         enum: ["tel", "text", "email"],
+  //     },
+  //     name: String,
+  //     label: String
+  // })
 
-    data_ = parse(html).map((el, i) => {
+  console.log(...parsedInputs);
+  const form = await new Form({
+    fields: parsedInputs,
+  }).save();
 
-            if (el.children[3].attributes[1].value == "name") {
-                obj.push({ "name": el.children[3].attributes[1].value })
-
-            } else if (el.children[3].attributes[1].value == "email") {
-                obj.push({ "email": el.children[3].attributes[1].value })
-
-            } else if (el.children[3].attributes[1].value == "tel") {
-                obj.push({ "tel": el.children[3].attributes[1].value })
-            }
-
-        })
-        /**    data_ = parse(html).map((el, i) => {
-
-            if (el.children[3].attributes[1].value == "name") {
-                obj.push({ "name": el.children[3].attributes[1].value })
-
-            } else if (el.children[3].attributes[1].value == "email") {
-                obj.push({ "email": el.children[3].attributes[1].value })
-
-            } else if (el.children[3].attributes[1].value == "tel") {
-                obj.push("tel": el.children[3].attributes[1].value)
-            }
-
-        }) */
-    console.log(data_)
-        //console.log(...data_)
-    const form = await new Form({
-        input: obj
-    }).save();
-
-    res.status(201).send({ id: form._id });
+  res.status(201).send({ id: form._id });
 };
 
-const getCreatedForm = async(req, res) => {
-    const { id } = req.params;
+const getCreatedForm = async (req, res) => {
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return false;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("Recurso no encontrado");
 
-    const form = await Form.findOne({ _id: id })
-    res.status(200).render("forms/id", { form })
+  const form = await Form.findOne({ _id: id });
+  res.status(200).render("forms/id", { form });
 
-    // try {
-    //       const form = await Form.findOne({ _id: id });
+  // try {
+  //       const form = await Form.findOne({ _id: id });
 
-    //       res.status(200).render("forms/id", { form });
-    //   } catch (error) {
-    //       console.error(error);
-    //       res.status(404).send("Recurso no existente");
-    //   }
+  //       res.status(200).render("forms/id", { form });
+  //   } catch (error) {
+  //       console.error(error);
+  //       res.status(404).send("Recurso no existente");
+  //   }
 };
 
 module.exports = { createForm, createNewForm, getCreatedForm };
--- models/forms.js
+++ models/forms.js
@@ -1,25 +1,16 @@
 const { Schema, model } = require("mongoose");
 
-const newFormSchema = new Schema({
-    input: [{
-        "name": [{
-            type: [String],
-            maxlength: 20
-        }],
-        "tel": {
-            type: [String],
-            maxlength: 9
-        },
-        "email": {
-            type: [String],
-        }
-    }]
-})
-
-
-const formSchema = new Schema({
-    html: String,
-    slug: String,
+const fieldSchema = new Schema({
+  input: {
+    type: [String],
+    enum: ["tel", "text", "email"],
+  },
+  name: [String],
+  label: [String],
 });
 
-module.exports = model("form", newFormSchema);
+const formSchema = new Schema({
+  fields: [fieldSchema],
+});
+
+module.exports = model("form", formSchema);
