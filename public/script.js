35e0b1cbd382bb669f848eb2077c4e6ae716b127
Joao <joaoyutubesilva2000@gmail.com>
2022-02-01 14:39:04 +0100

    issue completada

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const dragZone = document.querySelector("#drag-from-me-babe"); // Contenedor del
 const htmlCode = document.querySelector("#html-form-code"); // Código HTML generado, lo guardaríamos en la base de datos
 const butonCreateForm = document.querySelector("#boton-form");
 let numControls = 0;
+let url = ``;
 
 function enableForButton(enable) {
   if (enable) {
@@ -11,6 +12,14 @@ function enableForButton(enable) {
     document.querySelector("#boton-form").className =
       "btn btn-primary disabled";
   }
+}
+
+async function handleClick() {
+  /* Save value of myText to input variable */
+  var input = url;
+
+  /* Copy the text inside the text field */
+  await navigator.clipboard.writeText(input);
 }
 
 new Sortable(dropZone, {
@@ -51,8 +60,9 @@ const createFeedbackMessage = (type, message) => {
   wrapper.innerHTML =
     '<div class="alert alert-' +
     type +
-    ' alert-dismissible" role="alert">' +
-    message;
+    ' alert-dismissible  position-relative" role="alert">' +
+    message +
+    ' <a href="#" class="pe-auto" title="Copy"><svg xmlns="http://www.w3.org/2000/svg" id="copy" onclick="handleClick()" width="25" height="25" fill="currentColor" class="bi bi-clipboard-plus position-absolute top-50 end-0 translate-middle-y" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg></a>';
 
   alertPlaceholder.append(wrapper);
 };
@@ -76,6 +86,7 @@ document.querySelector("button").addEventListener("click", async () => {
     }
 
     const data = await response.json(); // Obtenemos los datos del servidor
+    url = `http://localhost:3000/forms/${data.id}`;
     const successMessage = `¡Formulario creado correctamente! Puedes visualizarlo en <a href="http://localhost:3000/forms/${data.id}" class="alert-link" target="_blank">http://localhost:3000/forms/${data.id}</a>.`;
     // Si todo ha ido bien, confirmamos la creación del formulario
     createFeedbackMessage("success", successMessage);
